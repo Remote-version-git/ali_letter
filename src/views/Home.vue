e2abca259033845a09c94a3d726a3264c3704f97
ZhouYing-123 <1334709558@qq.com>
2019-10-15 20:21:39 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <h1>111</h1>
-  </div>
+  <div class="home">this is home</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
+
+<style>
+</style>
