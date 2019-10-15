a693cd119062bc2a60c6a91597499aa644dc4ffe
ChangKeXin <1296208508@qq.com>
2019-10-15 20:12:54 +0800

    chengkexin1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from "./router";
 import store from "./store";
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +13,7 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
+
+
