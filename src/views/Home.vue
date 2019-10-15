0f4ff10a90eec7d9d8918447083d3c06f25a8246
jiangting <jiangting@qq.com>
2019-10-15 20:35:48 +0800

    jiangting1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="home">this is home</div>
+  <div class="index">
+    <!-- 布局 -->
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -7,4 +14,10 @@ export default {};
 </script>
 
 <style>
+ .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
 </style>
