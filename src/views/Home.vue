ccf1938066c11b8b87589121c09b413d77f78910
jiangting <jiangting@qq.com>
2019-10-16 08:46:00 +0800

    蒋婷

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -82,7 +82,7 @@
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
 /* 公共宽度 */
 .common {
   width: 1120px;
@@ -94,7 +94,7 @@ export default {};
 
 
 /* 导航选中样式 */
-.active {
+.class_active {
   width: 110px;
   height: 60px;
   border-bottom: 3px solid darkblue;
