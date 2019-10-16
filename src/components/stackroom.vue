283b4988e3a9cb293b25de10a6f6b13a60079d54
ZhouYing-123 <1334709558@qq.com>
2019-10-16 22:29:17 +0800

    ZhouYing's work

--- src/components/stackroom.vue
+++ src/components/stackroom.vue
@@ -10,34 +10,138 @@
         </el-carousel>
       </div>
     </div>
-    <div class="roomcentre">
-      <!-- card卡片组件 -->
-      <el-card class="box-card">
-        <dt>衍生方向</dt>
-        <dd>全部</dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-        <dd></dd>
-      </el-card>
+    <div class="books_classify">
+      <div class="roomcentre">
+        <!-- card卡片组件 -->
+        <el-card class="box-card">
+          <div class="satck_direction satck_direction1 clearfix">
+            <h3>衍生方向</h3>
+            <ul>
+              <li>
+                <a href>全部</a>
+              </li>
+              <li>
+                <a href class="HaoJiHua">HAO计划</a>
+              </li>
+              <li>
+                <a href>电视剧</a>
+              </li>
+              <li>
+                <a href>网剧</a>
+              </li>
+              <li>
+                <a href>游戏</a>
+              </li>
+              <li>
+                <a href>电影</a>
+              </li>
+              <li>
+                <a href>漫画</a>
+              </li>
+              <li>
+                <a href>出版</a>
+              </li>
+              <li>
+                <a href>其他</a>
+              </li>
+            </ul>
+          </div>
+          <div class="satck_direction clearfix">
+            <h3>作品分类</h3>
+            <ul>
+              <li>
+                <a href>全部</a>
+              </li>
+              <li>
+                <a href>好计划</a>
+              </li>
+              <li>
+                <a href>电视剧</a>
+              </li>
+              <li>
+                <a href>网剧</a>
+              </li>
+              <li>
+                <a href>游戏</a>
+              </li>
+              <li>
+                <a href>电影</a>
+              </li>
+              <li>
+                <a href>漫画</a>
+              </li>
+              <li>
+                <a href>出版</a>
+              </li>
+              <li>
+                <a href>其他</a>
+              </li>
+            </ul>
+          </div>
+          <div class="satck_direction clearfix">
+            <h3>版权状态</h3>
+            <ul>
+              <li>
+                <a href>全部</a>
+              </li>
+              <li>
+                <a href>好计划</a>
+              </li>
+              <li>
+                <a href>电视剧</a>
+              </li>
+              <li>
+                <a href>网剧</a>
+              </li>
+              <li>
+                <a href>游戏</a>
+              </li>
+              <li>
+                <a href>电影</a>
+              </li>
+              <li>
+                <a href>漫画</a>
+              </li>
+              <li>
+                <a href>出版</a>
+              </li>
+              <li>
+                <a href>其他</a>
+              </li>
+            </ul>
+          </div>
+        </el-card>
+      </div>
+    </div>
+    <!-- 书籍展示部分开始 -->
+    <div class="books_detail">
+      <div class="roomcentre">
+        <!-- tabs标签页组件 -->
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="按时间排序" name="first">用户管理</el-tab-pane>
+          <el-tab-pane label="按热度排序" name="second">配置管理</el-tab-pane>
+        </el-tabs>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      activeName: "second"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-// .stackroom {
-// //   height: 500px;
-//   //   background-color: #000;
-// }
-
 .roomcentre {
   width: 1120px;
   margin: 0 auto;
@@ -65,8 +169,78 @@ export default {};
 }
 
 // card卡片组件
-.el-card {
-  margin-top: 30px;
-  border-top: 1px solid #f89a3b;
+.books_classify {
+  background-color: #f7f8fa;
+  padding: 30px 0 1px 0;
 }
+
+.el-card {
+  height: 145px;
+  border-top: 1px solid #f89a3b;
+  margin-bottom: 30px;
+}
+
+// 分类部分
+.satck_direction {
+  display: inline-block;
+  margin-bottom: 15px;
+}
+
+.satck_direction > h3,
+.satck_direction > ul,
+.satck_direction > ul > li {
+  float: left;
+}
+
+.satck_direction > h3 {
+  color: #8e96aa;
+  width: 90px;
+  height: 24px;
+  line-height: 24px;
+  font-size: 14px;
+  font-family: "Microsoft YaHei";
+  font-weight: normal;
+}
+
+.satck_direction > ul > li {
+  height: 24px;
+  line-height: 24px;
+}
+
+.satck_direction > ul > li:nth-child(1) {
+  color: #ffffff;
+  background-color: #ff910d;
+  border-radius: 20px;
+}
+
+.satck_direction > ul > li a {
+  font-size: 14px;
+  padding: 0 10px;
+  font-family: "Microsoft YaHei";
+}
+
+.HaoJiHua {
+  font-weight: 900;
+  color: red;
+}
+
+// 书籍展示部分
+.books_detail {
+  height: 600px;
+  //   background-color: pink;
+}
+
+.el-tabs__item {
+  height: 44px;
+  line-height: 44px;
+  font-size: 14px;
+  font-family: "Microsoft YaHei";
+}
+
+.el-tabs {
+  margin-top: 20px;
+  margin-bottom: 40px;
+}
+
+
 </style>
