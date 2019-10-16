46ba4b7f9794b97bc558dd0e1524a55bf9a43a55
jiangting <jiangting@qq.com>
2019-10-16 08:40:15 +0800

    蒋婷

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,14 @@
   <div class="index">
     <!-- 布局 -->
     <el-container>
-
-      <el-header>
+      <el-header style="height:100%">
         <!-- 顶部 -->
         <div class="common top">
           <a href="/" class="top-left">
             <img src="../assets/img/logo.png" alt />
           </a>
 
-          <div class="top_right">
+          <div class="top_right clear">
             <ul>
               <li>
                 <a href>下载客户端</a>
@@ -35,29 +34,44 @@
               </li>
             </ul>
           </div>
-        </div>
 
-        <!-- 导航栏 -->
-        <div class="nav">
-          <div class="nav_classification">
-            <ul>
-              <li><a href="">首页</a></li>
-              <li><a href="">男频</a></li>
-              <li><a href="">女频</a></li>
-              <li><a href="">轻小说</a></li>
-              <li><a href="">书库</a></li>
-              <li><a href="">排行榜</a></li>
-            </ul>
+          <!-- 导航栏 -->
+          <div class="nav clear">
+            <div class="nav_classification">
+              <ul>
+                <li class="active">
+                  <a href>首页</a>
+                </li>
+                <li>
+                  <a href>男频</a>
+                </li>
+                <li>
+                  <a href>女频</a>
+                </li>
+                <li>
+                  <a href>轻小说</a>
+                </li>
+                <li>
+                  <a href>书库</a>
+                </li>
+                <li>
+                  <a href>排行榜</a>
+                </li>
+              </ul>
+            </div>
+            <div class="nav_search">
+              <el-input v-model="input" placeholder="请根据书名或者作者" width="20px">
+                <el-button icon="el-icon-search" circle slot="append"></el-button>
+              </el-input>
+            </div>
           </div>
         </div>
       </el-header>
-      <!-- <el-main class="common">Main</el-main> -->
-      <!-- <el-footer class="common">Footer</el-footer> -->
 
-      <el-header>Header</el-header>
-      <el-main><router-view></router-view></el-main>
-      <el-footer>Footer</el-footer>
-
+      <el-main style="height:100%">
+        <router-view></router-view>
+      </el-main>
+      <el-footer style="height:100%"></el-footer>
     </el-container>
   </div>
 </template>
@@ -75,9 +89,18 @@ export default {};
 }
 
 /* 布局css */
-.el-header,
-.el-footer {
-  line-height: 60px;
+
+
+/* 导航选中样式 */
+.active {
+  width: 110px;
+  height: 60px;
+  border-bottom: 3px solid darkblue;
+}
+
+/* 清除浮动 */
+.clear {
+  clear: both;
 }
 
 /* 顶部 */
@@ -96,6 +119,7 @@ export default {};
 .top_right {
   float: right;
   height: 60px;
+  line-height: 60px;
 }
 
 .top_right ul {
@@ -107,10 +131,48 @@ export default {};
 }
 
 .top_right ul li > a {
-    color: #333;
-    font-size: 13px;
-    font-weight: bold;
+  color: #333;
+  font-size: 13px;
+  font-weight: bold;
 }
 /* 顶部结束 */
 
+/* 导航 */
+.nav_classification {
+  display: inline-block;
+}
+
+.nav ul li {
+  display: inline-block;
+  width: 110px;
+  height: 60px;
+  text-align: center;
+  line-height: 60px;
+}
+
+.nav ul li a {
+  display: inline-block;
+  font-size: 18px;
+}
+
+.nav ul li a:hover {
+  color: #ff6c17;
+}
+
+.nav_search {
+  display: inline-block;
+  width: 300px;
+  float: right;
+  line-height: 60px;
+}
+
+.el-input__inner {
+  border-radius: 50px;
+  height: 28px;
+}
+
+.el-input-group__append {
+  border-radius: 0px 50px 50px 0;
+  background-color: transparent;
+}
 </style>
