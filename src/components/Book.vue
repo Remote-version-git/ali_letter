2c51be469cca20434dd3b83b6d0a40a6125e468a
ChangKeXin <1296208508@qq.com>
2019-10-16 20:05:05 +0800

    1016

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -56,8 +56,212 @@
         </ul>
       </div>
     </div>
+    <div class="zong">
+      <div class="leftarea">
+        <div class="lastchapter" data-clog="base">
+          <div class="titleline">
+            <span class="title">最新章节</span>
+          </div>
+          <a target="_blank" href>
+            <div class="chapname">第3488章 突破地仙中期</div>
+            <div class="chapcont">
+              <i></i>
+            </div>
+          </a>
+        </div>
+        <div class="comment-view js-commentView" data-clog="comment">
+          <div class="titleline">
+            <span class="title">
+              书友评论
+              <i data-spm-anchor-id="aliwx.cover.0.i2.69e510ceAqk028">（ 701856条 ）</i>
+            </span>
+            <span class="btn">
+              <a
+                target="_blank"
+                href="/comment?bookid=6725806&amp;authorid=1930553"
+                data-clog="all-all$$bid=6725806"
+              >全部评论</a>
+            </span>
+          </div>
+          <!-- <p class="pub-warning hide js-pubWarning">内容正在审核中，请耐心等待...</p> -->
+          <div class="publish js-showPublishArea" data-clog="publish-publish$$bid=6725806">登录才能评论噢~</div>
+          <!-- 登录后发表评论 -->
+          <!-- <div class="writearea hide js-commentTextarea">
+            <textarea placeholder="发表评论"></textarea>
+            <div class="textinfo">
+              <span class="wordcount js-words"></span>
+              <span class="notice js-notice"></span>
+              <span class="submit js-sendComment" data-clog="send-send$$bid=6725806">发送</span>
+              <span class="abort js-commentAbort">取消</span>
+            </div>
+          </div>-->
+
+          <ul class="comments js-commentList">
+            <li class="clear">
+              <div class="comment">
+                <div class="photo">
+                  <img
+                    src="http://img-tailor.11222.cn/account/avatar/90a99ecd2f3e090c01ce85f99e232e0e.bmp"
+                  />
+                </div>
+                <div class="username">
+                  花幽山月
+                  <span class="flag author">作者</span>
+                  <span class="flag top">置顶</span>
+                  <span class="flag good">精</span>
+                </div>
+
+                <p class="content js-textArea">
+                  山月建了几个交流群，大家可以选择性的加入。
+                  VIP①群：730498640（大群，高素质）
+                  VIP②群：723437261（暂时备用，①群满员后可入）
+                  VIP③群：745180129（备用）
+                  VIP书友群只加书旗正版用户，需订阅截图。
+                  介于山月更新时间经常不稳定，我会在群里通知更新每日更新时间，偶尔也会在群里冒泡聊聊后续情节。
+                  公众书友①群：190705584（大群，整合了贴吧群）
+                  公众书友②群：...
+                </p>
+                <div class="more">
+                  <p
+                    class="js-showAll"
+                    data-text="山月建了几个交流群，大家可以选择性的加入。
+VIP①群：730498640（大群，高素质）
+VIP②群：723437261（暂时备用，①群满员后可入）
+VIP③群：745180129（备用）
+VIP书友群只加书旗正版用户，需订阅截图。
+介于山月更新时间经常不稳定，我会在群里通知更新每日更新时间，偶尔也会在群里冒泡聊聊后续情节。
+公众书友①群：190705584（大群，整合了贴吧群）
+公众书友②群：729886658
+公众书友③群：749740054（备用）
+大家可以选择性加入，建立交流群只是为了更方便和大家交流，并及时告诉大家更新时间，免得总是让读者等。
+群里严禁黄图，喷作者可以允许，但不要辱骂群里书友，和谐相处。
+能给大家一段轻松愉悦的阅读记忆，山月心中也会有不小成就感，希望各位道友能踊跃交流，畅所欲言。
+PS：微信书群可以让群友拉进去。"
+                  >显示全文</p>
+                </div>
+                <div class="info">
+                  <span class="time">2018-05-12 01:15</span>
+                  <span class="reply js-checkoutReply">1065</span>
+                  <span class="line"></span>
+                  <span class="good js-goodComment">20305</span>
+                </div>
+              </div>
+              <div class="js-replysView"></div>
+            </li>
+
+            <li class="clear">
+              <div class="comment">
+                <div class="photo">
+                  <img
+                    src="https://tfs.alipayobjects.com/images/partner/TB1ll1SXwSJDuNjmgXqXXbesFXa"
+                  />
+                </div>
+                <div class="username">
+                  凡哥
+                  <span class="flag good">精</span>
+                </div>
+                <p class="content js-textArea">
+                  越写越烂，废话连篇，剧情推进速度简直比蜗牛还慢 追了这么久了真的让人很失望
+                  <span
+                    class="comments-face"
+                    style="background-position:87.5% 0%;"
+                  ></span>
+                </p>
+                <div class="info">
+                  <span class="time">2天前</span>
+                  <span class="reply js-checkoutReply">0</span>
+                  <span class="line"></span>
+                  <span class="good js-goodComment">58</span>
+                </div>
+              </div>
+              <div class="js-replysView"></div>
+            </li>
+
+            <li class="clear">
+              <div class="comment">
+                <div class="photo">
+                  <img
+                    src="http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLnJXXzCXI1aRTrc2QZRyMn3v0QZHSdCYNmSr7FPSXpyG2jjcpR9JgyT3zOPSWia8ZnR1pwq89T2RQ/132"
+                  />
+                </div>
+                <div class="username">
+                  蓝图灯光音响心无杂念
+                  <span class="flag good">精</span>
+                </div>
+
+                <p class="content js-textArea">上个台阶写5天</p>
+
+                <div class="info">
+                  <span class="time">2天前</span>
+                  <span class="reply js-checkoutReply">0</span>
+                  <span class="line"></span>
+                  <span class="good js-goodComment">57</span>
+                </div>
+              </div>
+              <div class="js-replysView"></div>
+            </li>
+
+            <li class="clear">
+              <div class="comment">
+                <div class="photo">
+                  <img
+                    src="http://img-tailor.11222.cn/account/avatar/25cca6c168f3eeb7ff01ed1d3be99baa@120w_120h"
+                  />
+                </div>
+                <div class="username">
+                  书友1061022006
+                  <span class="flag good">精</span>
+                </div>
+
+                <p class="content js-textArea">雷霆漩涡中黑白紫青黑共五种雷霆能量。。。呵呵，作者又花了半章形容五雷法，我已经会背了，谢谢作者。</p>
+
+                <div class="info">
+                  <span class="time">2天前</span>
+                  <span class="reply js-checkoutReply">3</span>
+                  <span class="line"></span>
+                  <span class="good js-goodComment">54</span>
+                </div>
+              </div>
+              <div class="js-replysView"></div>
+            </li>
+          </ul>
+          <div class="btn-toall">
+            <a href="#">查看全部 &gt;</a>
+          </div>
+        </div>
+      </div>
+      <!-- 右边 -->
+      <div class="rightarea">
+        <div class="user-orther">
+          <div class="header">看过这本书的还看过</div>
+          <ul class="clear">
+            <li>
+              <a href="#">
+                <p class="title">老夫少妻的互撩日常</p>
+              </a>
+              <p class="auth">柳赋雨</p>
+              <p class="desc">《霸总夫妇的互撩日常》已发，本书姐妹篇，求支持。家族企业狗带，私生女江梦娴被八万块卖给了一个老光棍做</p>
+            </li>
+
+            <li>
+              <a href="#">
+                <p class="title">第一战神</p>
+              </a>
+              <p class="auth">我本纯洁</p>
+              <p class="desc">千万级销量作家我本纯洁火爆玄幻作品来袭——少年杨武遭美女陷害入狱，侥幸获神犬之助，破狱场，驰沙场，揽</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+export default {};
+</script>
+
 <style >
 .zong {
   width: 1120px;
@@ -200,5 +404,322 @@ img.cover {
   border-right: 1px solid #ebebeb;
   color: #999;
   margin-right: 15px;
+}
+
+/* 最新章节 */
+.leftarea {
+  width: 77.86%;
+  float: left;
+  margin-top: 24px;
+  margin-bottom: 40px;
+}
+.leftarea .lastchapter {
+  color: #1d1e20;
+}
+.leftarea .lastchapter .titleline {
+  height: 22px;
+  line-height: 22px;
+  font-size: 18px;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.leftarea .lastchapter .titleline .title {
+  padding: 0 18px 0 14px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../assets/img/changkexinimg/title_icon_cate.png) no-repeat 0 0
+    #fff;
+  background-size: auto 22px;
+}
+.leftarea .lastchapter .chapname {
+  font-size: 16px;
+  height: 16px;
+  line-height: 18px;
+  margin: 10px 0;
+  padding-left: 14px;
+}
+.leftarea .lastchapter .chapcont {
+  padding-left: 14px;
+  font-size: 12px;
+  line-height: 20px;
+  color: #999;
+  max-height: 100px;
+  overflow: hidden;
+}
+.leftarea .lastchapter .titleline:before {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 4px;
+  background-color: #e8f3f1;
+  margin-top: 9px;
+}
+a:hover {
+  color: #ff8e15;
+}
+/* 书友评论 */
+.leftarea .comment-view .titleline {
+  height: 22px;
+  line-height: 22px;
+  font-size: 18px;
+  width: 100%;
+  overflow: hidden;
+  margin: 14px 0;
+}
+.leftarea .comment-view .titleline span.title {
+  padding: 0 18px 0 14px;
+  float: left;
+  background: url(../assets/img/changkexinimg/title_icon_cate.png) no-repeat 0 0
+    #fff;
+  background-size: auto 22px;
+  height: 22px;
+  overflow: hidden;
+}
+.leftarea .comment-view .titleline span.title i {
+  color: #999;
+  font-size: 14px;
+  font-style: normal;
+}
+.leftarea .comment-view .titleline span.btn {
+  float: right;
+  width: 80px;
+  height: 20px;
+  border: 1px solid #f37315;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  color: #f37315;
+  border-radius: 3px;
+}
+.leftarea .comment-view .titleline span.btn a {
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  color: #f37315;
+}
+/* 登录才能评论 */
+.leftarea .comment-view .publish {
+  height: 42px;
+  border: 1px solid #dcdcdc;
+  background: #f8f8f8;
+  color: #999;
+  line-height: 42px;
+  text-indent: 19px;
+  font-size: 14px;
+}
+.leftarea .comment-view div.writearea {
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid #dcdcdc;
+  background: #f8f8f8;
+  padding: 12px 16px;
+  margin-top: 10px;
+  color: #999;
+  font-size: 14px;
+}
+.leftarea .comment-view div.writearea textarea {
+  width: 100%;
+  height: 200px;
+  line-height: 20px;
+  background: transparent;
+  border: 0;
+  resize: none;
+}
+/* 评论 */
+.leftarea .comment-view .comments {
+  width: 100%;
+}
+.leftarea .comment-view .comments li {
+  margin-top: 12px;
+  padding-bottom: 14px;
+  border-bottom: 1px #e4e4e4 dashed;
+  word-wrap: break-word;
+}
+.leftarea .comment-view .comments li .comment {
+  width: 100%;
+  box-sizing: border-box;
+  padding-left: 84px;
+  position: relative;
+}
+.leftarea .comment-view .comments li .comment .photo {
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  overflow: hidden;
+}
+.leftarea .comment-view .comments li .comment .photo img {
+  width: 100%;
+  height: 100%;
+}
+.leftarea .comment-view .comments li .comment .username {
+  font-size: 16px;
+  line-height: 32px;
+  height: 32px;
+  vertical-align: middle;
+  position: relative;
+  z-index: 2;
+}
+.leftarea .comment-view .comments li .comment .username .flag.author {
+  border-color: #dd641d;
+  background-color: #f36f20;
+}
+.leftarea .comment-view .comments li .comment .username .flag.top {
+  border-color: #e26580;
+  background-color: #f9708d;
+}
+.leftarea .comment-view .comments li .comment .username .flag.good {
+  border-color: #e89568;
+  background-color: #ffa473;
+}
+.leftarea .comment-view .comments li .comment .username .flag {
+  font-size: 12px;
+  padding: 2px 3px;
+  vertical-align: middle;
+  color: #fff;
+  border: 1px solid #fff;
+  display: inline-block;
+  line-height: 12px;
+  border-radius: 3px;
+  transform: translateY(-2px);
+  margin-right: 8px;
+}
+
+.leftarea .comment-view .comments li .comment .content {
+  line-height: 25px;
+  font-size: 15px;
+  width: 100%;
+  color: #999;
+  text-align: justify;
+  /* height: 99px;
+  overflow: hidden; */
+}
+/* 展示全文 */
+.leftarea .comment-view .comments li .comment .more {
+  height: 14px;
+  line-height: 14px;
+  font-size: 14px;
+}
+.leftarea .comment-view .comments li .comment .more p {
+  float: right;
+  color: #f36f20;
+  cursor: pointer;
+}
+.leftarea .comment-view .comments li .comment .more p:after {
+  content: "";
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: url(../assets/img/changkexinimg/item_more_flag.png);
+  margin-left: 4px;
+}
+/* 日期 */
+.leftarea .comment-view .comments li .comment .info {
+  line-height: 18px;
+  height: 18px;
+  font-size: 14px;
+  margin-top: 6px;
+}
+.leftarea .comment-view .comments li .comment .info .time {
+  float: left;
+  color: #999;
+}
+.leftarea .comment-view .comments li .comment .info .reply,
+.leftarea .comment-view .comments li .comment .info .good {
+  float: right;
+  padding-left: 24px;
+  position: relative;
+  z-index: 1;
+  cursor: pointer;
+}
+.leftarea .comment-view .comments li .comment .info .reply:before,
+.leftarea .comment-view .comments li .comment .info .good:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 18px;
+  height: 18px;
+  background: url(../assets/img/changkexinimg/item_flags.png) no-repeat 0 0;
+}
+.leftarea .comment-view .comments li .comment .info .good:before {
+  background-position: -20px 0;
+}
+.leftarea .comment-view .comments li .comment .info .line {
+  height: 100%;
+  width: 1px;
+  background: #e2e2e2;
+  margin: 0 9px;
+  float: right;
+}
+/* 查看全部 */
+.leftarea .comment-view .btn-toall {
+  font-size: 14px;
+  height: 16px;
+  line-height: 16px;
+  text-align: center;
+  margin-top: 20px;
+  color: #f37315;
+}
+.leftarea .comment-view .btn-toall a {
+  color: #f37315;
+}
+
+/* 右边 */
+.rightarea {
+    width: 220px;
+    float: right;
+    margin-top: 24px;
+}
+.rightarea .user-orther {
+    overflow: hidden;
+    color: #1c0800;
+    margin-top: 14px;
+}
+.rightarea .user-orther .header {
+    font-size: 16px;
+    line-height: 22px;
+    height: 22px;
+    overflow: hidden;
+    margin-bottom: 14px;
+    padding-left: 14px;
+    background: url(../assets/img/changkexinimg/title_icon_cate.png) no-repeat 0 0 #fff;
+    background-size: auto 22px;
+}
+.rightarea .user-orther li {
+    width: 100%;
+    height: 84px;
+    margin-bottom: 14px;
+    box-sizing: border-box;
+    padding-left: 8px;
+    border-left: 4px solid #eee;
+    position: relative;
+}
+.rightarea .user-orther li p.auth {
+    color: #999;
+    margin: 7px 0 9px;
+}
+.rightarea .user-orther li p.title, .rightarea .user-orther li p.auth {
+    font-size: 14px;
+    line-height: 16px;
+    height: 16px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.rightarea .user-orther li p.desc {
+    font-size: 12px;
+    line-height: 20px;
+    height: 40px;
+    color: #999;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
 }
 </style>
